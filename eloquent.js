5adeca3f002178aebc8363339276a5d022da02d7
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-07-29 18:02:29 +0100

    FizzBuzz exercise

--- eloquent.js
+++ eloquent.js
@@ -9,5 +9,23 @@ function loopTriangle(){
         condition += 1;
     }
 }
-
 //loopTriangle()
+
+// Second exercise : Fizzbuzz
+function fizzBuzz(){
+    for (let index = 0; index <= 100; index++) {
+        if (index % 3 === 0 && index % 5 === 0) {
+            console.log(index + ' FizzBuzz');
+        }
+        else if(index % 5 === 0){
+            console.log(index + ' Buzz');
+        }
+        else if(index % 3 === 0){
+            console.log(index + ' Fizz');
+        }
+        else{
+            continue;
+        };
+    }
+}
+fizzBuzz()
